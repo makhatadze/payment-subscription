303a1a322649e5e3e12f6cb80a9222018279c682
makhatadze <vitomaxatadze@gmail.com>
2021-06-10 14:28:04 +0400

    Make coupon migration.

---
+++ database/migrations/2021_06_10_102634_create_coupons_table.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *  database/migrations/2021_06_10_102634_create_coupons_table.php
+ *
+ * Date-Time: 10.06.21
+ * Time: 14:27
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateCouponsTable
+ */
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->timestamp('ends_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
