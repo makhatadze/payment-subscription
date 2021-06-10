ee9da55fd92a9503411bf07721a2459b6172f364
makhatadze <vitomaxatadze@gmail.com>
2021-06-10 14:07:32 +0400

    Make custom error validation.

---
+++ app/Http/Controllers/ActionController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *  app/Http/Controllers/ActionController.php
+ *
+ * Date-Time: 10.06.21
+ * Time: 14:00
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * Class ActionController
+ * @package App\Http\Controllers
+ */
+class ActionController extends Controller
+{
+    /**
+     * ActionController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return mixed
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function action(Request $request) {
+        $user = $request->user();
+
+        $this->validate($request,[
+            'password' => [
+                'required',
+                function ($attribute, $value, $fail) use($user) {
+                    if(!Hash::check($value,$user->password)) {
+                        $fail('Current password is incorrect');
+                    }
+                }
+            ]
+        ]);
+
+        return back()->withStatus('Passed');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,34 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <form method="POST" action="{{ route('action') }}">
+                    @csrf
 
-                    {{ __('You are logged in!') }}
-                </div>
+                    <div class="form-group row">
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="current-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Perform action') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
             </div>
         </div>
     </div>
